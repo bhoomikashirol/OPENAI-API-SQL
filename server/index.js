0d09b4073021ae03a1b09907238e1b0091877226
bhoomika shirol <bhoomikas0552@gmail.com>
2023-08-08 12:15:19 +0530

    gaa

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+const port = process.env.PORT || 3005;
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World! from our API");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
