e198a63a992373fef83af71e32cc5ad7fce2557a
bhoomika shirol <bhoomikas0552@gmail.com>
2023-08-10 17:28:12 +0530

    feat: add index.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,33 @@
-import styles from './index.module.css'
-import sqlServer from './assets/sql-logo.png'
 import { useState } from "react";
+import sqlLogo from './assets/sql-logo.png'
+import styles from './index.module.css'
 
-function App() {
-  const [queryDescription, setQueryDescription] = useState("");
+function App(){
+  const [count, setCount] = useState(0)
+  const [queryDescription, setQueryDescription] = useState("")
 
-  const onSubmit = (e) => {
+  const onSubmit =(e) => {
     e.preventDefault();
     console.log("form submitted: ", queryDescription);
-  };
+  }
+  return(
+  <main className={styles.main}>
+    <img src={sqlLogo} alt="" className={styles.icon}></img>
+    <h3>Generate SQL with AI</h3>
 
-  return (
-  <main> className= {styles.main}
-  <img src={sqlLogo} alt="" className={styles.icon} />
-  <h3>Generate SQL with AI</h3>
-  
-  <form onSubmit={onSubmit}>
-    <input 
-        type="text"
-        name="query-description" 
-        placeholder="Describe your query"
-          value={userPrompt}
-          onChange={(e) => setQueryDescription(e.target.value)}
-    />
+    <form onSubmit={onSubmit}>
+      <input type="text"
+      name="query-description"
+      placeholder="Describe your query"
+      onChange ={(e)=>  setQueryDescription(e.target.value)}>
 
-     <input type="submit" value="Generate query" /> 
-  </form>
-  <pre>{sqlQuery}</pre>
-  
+      </input>
+      
+      <input type = "submit" value="Generate query"/>
+    </form>
+
   </main>
-
-  );
+)
 }
 
-export default App();
+export default App
