a5240752f90520324b55fa25d63d110d27174f81
bhoomika shirol <bhoomikas0552@gmail.com>
2023-08-10 21:53:23 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,17 +1,32 @@
 import express from "express";
 import cors from "cors";
 
-
 const app = express();
+app.use(express.json()); 
 
-app.use(cors());
+app.use(cors(
+  { }
+));
 
 const port = process.env.PORT || 3005;
 
+import generate from "./generate.js";
+
 app.get("/", (req, res) => {
-  res.send("Hello World! from our API");
+  res.send("Hello World from our API!");
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
+});
+
+app.post("/generate", async (req, res) => {
+  const { queryDescription } = req.body
+  try {
+    const sqlQuery = await generate(queryDescription);
+    res.json({ response: sqlQuery });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
